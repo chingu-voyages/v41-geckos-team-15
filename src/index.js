c4d52cc858b4ed2843dfcf54951c4f74cedec9f5
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-10-24 15:40:24 +0400

    made css reset and added color/font variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './core-ui/index.css';
+import './core-ui/reset.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
