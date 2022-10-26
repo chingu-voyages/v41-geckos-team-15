e15d4fda9e3645d8f2922b2eb31df608a50c00e9
NawalKH <ezma1996@gmail.com>
2022-10-26 08:26:02 +0300

    Add conditions for landing nav and footer

--- src/components/landing-footer/LandingFooter.js
+++ src/components/landing-footer/LandingFooter.js
@@ -5,54 +5,60 @@ import Respository from '../../assets/images/socials/repository.png';
 import Website from '../../assets/images/socials/website.png';
 import MaleIcon from '../../assets/images/socials/male-icon.png';
 import FemaleIcon from '../../assets/images/socials/female-icon.png';
+import { useLocation } from 'react-router-dom';
 
 
 
 
 const LandingFooter = () => {
-    return (
-        <footer>
-            <article className='footer-article-one'>
-                <section className='footer-menus'>
-                    <img className='footer-logo' src={ExpenselessLogo} alt='Expenseless logo' />
-                    <nav>
-                        <ul className='footer-menu'>
-                            <li><Link to='/about'>About</Link></li>
-                            <li><Link to='/pricing'>Pricing</Link></li>
-                            <li><Link to='/faq'>FAQ</Link></li>
-                            <li><Link to='/blog'>Blog</Link></li>
-                            <li><Link to='/contact'>Contact</Link></li>
-                        </ul>
-                    </nav>
-                    <nav>
-                        <ul className='socials-menu'>
-                            <li><a href='https://github.com/chingu-voyages/v41-geckos-team-15'> <img src={Respository} alt='Github icon' /></a></li>
-                            <li><a href='/'> <img src={Website} alt='Website icon' /></a></li>
-                            <li><a href='https://github.com/NawalKH'> <img src={MaleIcon} alt='Male icon' /></a></li>
-                            <li><a href='https://github.com/catherineisonline'> <img src={FemaleIcon} alt='Female icon' /></a></li>
-                        </ul>
-                    </nav>
-                </section>
-                <form className='footer-form'>
-                    <label for="subscribe">Subscribe</label>
-                    <section className='form-input'>
-                        <input name='subscribe' type="text" placeholder='Enter your email' />
-                        <button type='button'>Subscribe</button>
+    const { pathname } = useLocation();
+    /*footer doesn't show when user go to dashboard*/
+    if (pathname === "/dashboard")
+        return <></>
+    else
+        return (
+            <footer>
+                <article className='footer-article-one'>
+                    <section className='footer-menus'>
+                        <img className='footer-logo' src={ExpenselessLogo} alt='Expenseless logo' />
+                        <nav>
+                            <ul className='footer-menu'>
+                                <li><Link to='/about'>About</Link></li>
+                                <li><Link to='/pricing'>Pricing</Link></li>
+                                <li><Link to='/faq'>FAQ</Link></li>
+                                <li><Link to='/blog'>Blog</Link></li>
+                                <li><Link to='/contact'>Contact</Link></li>
+                            </ul>
+                        </nav>
+                        <nav>
+                            <ul className='socials-menu'>
+                                <li><a href='https://github.com/chingu-voyages/v41-geckos-team-15'> <img src={Respository} alt='Github icon' /></a></li>
+                                <li><a href='/'> <img src={Website} alt='Website icon' /></a></li>
+                                <li><a href='https://github.com/NawalKH'> <img src={MaleIcon} alt='Male icon' /></a></li>
+                                <li><a href='https://github.com/catherineisonline'> <img src={FemaleIcon} alt='Female icon' /></a></li>
+                            </ul>
+                        </nav>
                     </section>
-                    <span>By subscribing you agree with our <Link to='/privacy'>Privacy Policy</Link></span>
-                </form>
-            </article>
-            <hr />
-            <article className='footer-article-two'>
-                <ul>
-                    <li><Link to='/privacy'>Privacy Policy</Link></li>
-                    <li><Link to='/terms'>Terms of Service</Link></li>
+                    <form className='footer-form'>
+                        <label for="subscribe">Subscribe</label>
+                        <section className='form-input'>
+                            <input name='subscribe' type="text" placeholder='Enter your email' />
+                            <button type='button'>Subscribe</button>
+                        </section>
+                        <span>By subscribing you agree with our <Link to='/privacy'>Privacy Policy</Link></span>
+                    </form>
+                </article>
+                <hr />
+                <article className='footer-article-two'>
+                    <ul>
+                        <li><Link to='/privacy'>Privacy Policy</Link></li>
+                        <li><Link to='/terms'>Terms of Service</Link></li>
 
-                </ul>
-                <p>2022 Expenseless. All rights reserved.</p>
-            </article>
-        </footer>
-    )
+                    </ul>
+                    <p>2022 Expenseless. All rights reserved.</p>
+                </article>
+            </footer>
+        )
 }
 
 export default LandingFooter;
--- src/components/landing-navigation/LandingNavigation.js
+++ src/components/landing-navigation/LandingNavigation.js
@@ -2,18 +2,23 @@ import ExpenselessLogo from '../../assets/images/expenseless-logo.jpg'
 import NavigationMenu from './NavigationMenu';
 import NavAction from './NavAction';
 import './landingNavigation.css';
+import { useLocation } from 'react-router-dom';
 
 const LandingNavigation = (props) => {
-    return (
-        <header className='landing-header'>
-            <img src={ExpenselessLogo} alt="Expenseless" />
-            <section className='nav-section'>
-                {/*when the user is logged in the About,Pricing..etc will not appear in the dashboard*/}
-                {!props.user && <NavigationMenu />}
-                <NavAction user={props.user} onLogout={props.onLogout} />
-            </section>
-        </header>
-    )
+    const { pathname } = useLocation();
+    /*landing nav bar doesn't show when user go to dashboard*/
+    if (pathname === "/dashboard" && props.user)
+        return <></>
+    else
+        return (
+            <header className='landing-header'>
+                <img src={ExpenselessLogo} alt="Expenseless" />
+                <section className='nav-section'>
+                    <NavigationMenu />
+                    <NavAction user={props.user} onLogout={props.onLogout} />
+                </section>
+            </header>
+        )
 }
 
 export default LandingNavigation;
