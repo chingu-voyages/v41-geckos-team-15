41e88fa7dd6083f183c9a744d756dc497e794b42
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-10-28 15:06:41 +0400

    added existing category descriptions

--- src/data/Categories.js
+++ src/data/Categories.js
@@ -38,61 +38,72 @@ const categories = [
         id: 4,
         name: "Shopping",
         cover: Shopping,
-        description: "Shopping spending details like clothing, jeans, bags, shoes."
+        description: "Shopping spending details like jewelery, bags and shoes."
     },
     {
         id: 5,
         name: "Restaurants",
-        cover: Restaurants
+        cover: Restaurants,
+        description: "Restaurant spending details like Burger King, McDonalds, Sushi Way, Dunkin Donuts."
     },
     {
         id: 6,
         name: "Utilities",
-        cover: Utilities
+        cover: Utilities,
+        description: "Utilities spending details like electricity, water and gas."
     },
     {
         id: 7,
         name: "Clothes",
-        cover: Clothes
+        cover: Clothes,
+        description: "Clothes spending details like sweater, jeans, socks or shorts."
     },
     {
         id: 8,
         name: "Gifts",
-        cover: Gifts
+        cover: Gifts,
+        description: "Gifts spending details like birthday gift for mom, Christmas gifts for friends or anniversary gifts."
     },
     {
         id: 9,
         name: "Travel",
-        cover: Travel
+        cover: Travel,
+        description: "Travel spending details like plane tickets, transportation and fees."
     },
     {
         id: 10,
         name: "Alcohol",
-        cover: Alcohol
+        cover: Alcohol,
+        description: "Alcohol spending details like beer, whiskey or wine."
     },
     {
         id: 11,
         name: "Health",
-        cover: Health
+        cover: Health,
+        description: "Health spending details like doctor visit, meds or equipment."
     },
     {
         id: 12,
         name: "Subscribtions",
-        cover: Subscribtions
+        cover: Subscribtions,
+        description: "Subscribtions spending details like Netflix, Apple Music or HBO."
     },
     {
         id: 13,
         name: "Savings",
-        cover: Savings
+        cover: Savings,
+        description: "Savings spending details like saving for travel, birthday or a new computer."
     }, {
         id: 14,
         name: "Services",
-        cover: Services
+        cover: Services,
+        description: "Services spending details like notary, lawyer or handyman."
     },
     {
         id: 15,
         name: "Investments",
-        cover: Investments
+        cover: Investments,
+        description: "Investments spending details like Forex, stocks or anything similar."
     },
 
 ]
