9b5ade43c1a34ffdd9790b7aabee4bc81f9bac37
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-10-30 11:01:07 +0400

    added profile page

--- src/routes/profile-dashboard/ProfileDashboard.js
+++ src/routes/profile-dashboard/ProfileDashboard.js
@@ -1,6 +1,48 @@
+import './profileDashboard.css'
+
 const ProfileDashboard = () => {
     return (
-        <div>I am ProfileDashboard</div>
+        <main className="profile-dashboard">
+            <article className="profile">
+                <h2>Profile</h2>
+                <ul>
+                    <li>
+                        <h3>Email</h3>
+                        <p>someemail@mail.com
+                        </p>
+                        <button>Edit</button>
+                    </li>
+                    <li>
+                        <h3>Password</h3>
+                        <p>*********************
+                        </p>
+                        <button>Edit</button>
+                    </li>
+                    <li>
+                        <h3>Language</h3>
+                        <p>English (US)
+                        </p>
+                        <button>Edit</button>
+                    </li>
+                    <li>
+                        <h3>Currency</h3>
+                        <p>USD
+                        </p>
+                        <button>Edit</button>
+                    </li>
+                    <li>
+                        <h3>Subscribtion</h3>
+                        <p>Basic
+                        </p>
+                        <button>Edit</button>
+                    </li>
+                </ul>
+                <section className='profile-buttons'>
+                    <button>Deactivate account</button>
+                    <button>Delete account</button>
+                </section>
+            </article>
+        </main>
     )
 }
 
