dfdf938ba3de011be02ccda937fe6cc93bbc87ae
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-10-31 18:53:05 +0400

    fixed styles of dashboard sidebar

--- src/components/dashboard-navigation/DashboardNavigation.js
+++ src/components/dashboard-navigation/DashboardNavigation.js
@@ -28,46 +28,42 @@ const DashboardNavigation = (props) => {
                     <li>
                         <Link to="accounts">
                             <Accounts className="icon" />
-                            <span className="item-name">accounts</span>
+                            <span className="item-name">Accounts</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="expenses">
                             <Expenses className="icon" />
-                            <span className="item-name">expenses</span>
+                            <span className="item-name">Expenses</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="budget">
                             <Budget className="icon" />
-                            <span className="item-name">budget</span>
+                            <span className="item-name">Budget</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="statistics">
                             <Statistics className="icon" />
-                            <span className="item-name">statistics</span>
+                            <span className="item-name">Statistics</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="profile">
                             <Profile className="icon" />
-                            <span className="item-name">profile</span>
+                            <span className="item-name">Profile</span>
                         </Link>
                     </li>
                 </ul>
 
 
-                <div class="bottom-content">
-                    <li class="">
-                        <Link to="/">
-                            <button className="logout-button" onClick={props.logout}>
-                                <Logout className="icon" />
-                                <span class="item-name">Logout</span>
-                            </button>
-                        </Link>
-                    </li>
-                </div>
+                {/* <div class="bottom-content"> */}
+                <button className="logout-button" onClick={props.logout}>
+                    <Logout className="icon" />
+                    <span class="item-name">Logout</span>
+                </button>
+                {/* </div> */}
             </div>
         </div>
     )
