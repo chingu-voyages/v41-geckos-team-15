cdf0444b48125a53e517ea974939df721089237c
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-10-31 20:14:09 +0400

    aded responsive design for expenses page

--- src/routes/expenses-dashboard/IncomesTable.js
+++ src/routes/expenses-dashboard/IncomesTable.js
@@ -5,21 +5,21 @@ const IncomesTable = (props) => {
         <table className="greyGridTable">
             <thead>
                 <tr>
-                    <th>Income name</th>
-                    <th>Category</th>
-                    <th>Created</th>
-                    <th>Amount</th>
-                    <th>More</th>
+                    <th scope="col">Income name</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Created</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">More</th>
                 </tr>
             </thead>
             <tbody>
                 {incomes.map((income) =>
                     <tr key={income.id}>
-                        <td> <img src={income.cover} alt="" />{income.name}</td>
-                        <td>{income.category}</td>
-                        <td>{income.created}</td>
-                        <td className="income-amount">{income.amount}{income.currency}</td>
-                        <td onClick={() => {
+                        <td data-label="Expense name"><span><img src={income.cover} alt="" />{income.name}</span></td>
+                        <td data-label="Category">{income.category}</td>
+                        <td data-label="Created">{income.created}</td>
+                        <td data-label="Amount" className="income-amount">{income.amount}{income.currency}</td>
+                        <td data-label="More" onClick={() => {
                             props.toggleExistingRecords();
                             props.getRecordInfo(income.name, income.category, income.amount, income.currency, income.created, income.note);
                         }}>...</td>
