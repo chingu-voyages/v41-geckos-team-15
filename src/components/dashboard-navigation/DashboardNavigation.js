df0781ef49d907bfc20cbba9842d378ff1213293
NawalKH <ezma1996@gmail.com>
2022-11-01 06:47:29 +0300

    fix dashboard side navbar

--- src/components/dashboard-navigation/DashboardNavigation.js
+++ src/components/dashboard-navigation/DashboardNavigation.js
@@ -8,70 +8,73 @@ import { ReactComponent as Budget } from '../../assets/images/dashboard-icons/bu
 import { ReactComponent as Profile } from '../../assets/images/dashboard-icons/profile.svg'
 import { ReactComponent as Logout } from '../../assets/images/dashboard-icons/log-out.svg'
 import ExpenselessLogo from '../../assets/images/expenseless-logo.jpg'
-import Hamburger from '../../assets/images/landing/hamburger.png'
-import { useState } from 'react';
+import { useState } from "react";
 
 const DashboardNavigation = (props) => {
-    const [toggleMenu, setToggleMeu] = useState(false);
-    const toggleHamMenu = () => {
-        toggleMenu ? setToggleMeu(false) : setToggleMeu(true);
-    }
-    const toggleOffMenu = () => {
-        setToggleMeu(false);
-    }
-    return (
+    const [responsive, setResponsive] = useState(false);
 
-        <div className={`sidebar${!toggleMenu ? `` : ` active-header`}`}>
-            <div className="logo" onClick={toggleOffMenu}>
+    const toggleMenu = () => setResponsive(!responsive)
+
+    return (
+        <div className={"sidebar " + (responsive && "is-active")}>
+            <div className={"menu-toggle " + (responsive && "is-active")} onClick={toggleMenu}>
+                <div className="dash-hamburger">
+                    <span></span>
+                </div>
+            </div>
+
+            <div className="logo">
                 <img src={ExpenselessLogo} alt="" />
             </div>
-            <img className='hamburger' src={Hamburger} alt="" onClick={toggleHamMenu} />
-            <div className={`sidebar-items${!toggleMenu ? `` : ` active-menu`}`}>
-                <ul>
+            <div className="sidebar-items">
+                <ul className="menu">
                     <li>
-                        <Link to="" onClick={toggleOffMenu}>
+                        <Link to="" onClick={toggleMenu}>
                             <Home className="icon" />
                             <span className="item-name">Home</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="accounts" onClick={toggleOffMenu}>
+                        <Link to="accounts" onClick={toggleMenu}>
                             <Accounts className="icon" />
-                            <span className="item-name">Accounts</span>
+                            <span className="item-name">accounts</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="expenses" onClick={toggleOffMenu}>
+                        <Link to="expenses" onClick={toggleMenu}>
                             <Expenses className="icon" />
-                            <span className="item-name">Expenses</span>
+                            <span className="item-name">expenses</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="budget">
+                        <Link to="budget" onClick={toggleMenu}>
                             <Budget className="icon" />
-                            <span className="item-name">Budget</span>
+                            <span className="item-name">budget</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="statistics" onClick={toggleOffMenu}>
+                        <Link to="statistics" onClick={toggleMenu}>
                             <Statistics className="icon" />
-                            <span className="item-name">Statistics</span>
+                            <span className="item-name">statistics</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="profile" onClick={toggleOffMenu}>
+                        <Link to="profile" onClick={toggleMenu}>
                             <Profile className="icon" />
-                            <span className="item-name">Profile</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/" className="logout-button" onClick={() => { props.logout(); toggleOffMenu() }} >
-                            <Logout className="icon" />
-                            <span className="item-name">Logout</span>
+                            <span className="item-name">profile</span>
                         </Link>
                     </li>
                 </ul>
-
+                <div class="bottom-content">
+                    <li class="">
+                        <Link to="/" onClick={props.logout}>
+                            <button className="logout-button" onClick={() => { props.logout(); toggleMenu() }}>
+                                <Logout className="icon" />
+                                <span class="item-name">Logout</span>
+                            </button>
+                        </Link>
+                    </li>
+                </div>
             </div>
         </div>
     )
