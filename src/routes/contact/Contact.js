41dd727a3f65ef89502889aef8cf955e510d510d
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-11-03 21:08:15 +0400

    added contact form validation

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -5,6 +5,7 @@ import Address from '../../assets/images/contact/address.png';
 import Phone from '../../assets/images/contact/phone.png';
 import Email from '../../assets/images/contact/email.png';
 import './leaflet.css'
+import { useState } from "react";
 
 //Leaflet
 import L from "leaflet";
@@ -23,27 +24,67 @@ const position = [40.7384638, -73.9896068];
 
 
 const Contact = () => {
+    const [formValue, setFormValue] = useState({ name: '', email: '', message: '' });
+    const [formError, setFormError] = useState({})
+    const [submit, setSubmit] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormError(validateForm(formValue))
+        setSubmit(true);
+        setFormValue({ name: '', email: '', message: '' });
+    }
+
+    const handleValidation = (e) => {
+        const { name, value } = e.target;
+        setFormValue({ ...formValue, [name]: value })
+    }
+
+    const validateForm = (value) => {
+        let errors = {}
+        const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if (!value.name) {
+            errors.name = "Please enter a valid name"
+        }
+        if (!value.email) {
+            errors.email = "Please enter email"
+        }
+        else if (!emailRegex.test(value.email)) {
+            errors.email = "Please enter valid email"
+        }
+        if (!value.message) {
+            errors.message = "Please enter a valid message"
+        }
+
+        return errors;
+    }
     return (
         <main className="contact">
             <article className="contact-us-form">
                 <h2>Contact us</h2>
                 <p>We are here to support you throughout your journey 24/7</p>
                 <section className="contact-us-form-section">
-                    <form>
-                        <label htmlFor="name">Name
-                            <input type="text" />
-                        </label>
-                        <label htmlFor="email">Email
-                            <input type="text" />
-                        </label>
-                        <label htmlFor="message">Message
-                            <textarea rows="5" type="text" /></label>
-                        <section className="terms-checkbox">
-                            <input type="checkbox" />
-                            <span>I accept the <Link to="terms">Terms</Link></span>
-                        </section>
-                        <button>Submit</button>
-                    </form>
+                    {submit ?
+                        <h3>You message has been sent successfully!</h3>
+                        : <form onSubmit={handleSubmit}>
+                            <label htmlFor="name">Name
+                                <input onChange={handleValidation} value={formValue.name} type="text" name="name" />
+                                <span className="contact-input-err">{formError.name}</span>
+                            </label>
+                            <label htmlFor="email">Email
+                                <input onChange={handleValidation} value={formValue.email} type="text" name="email" />
+                                <span className="contact-input-err">{formError.email}</span>
+                            </label>
+                            <label htmlFor="message">Message
+                                <textarea onChange={handleValidation} value={formValue.message} rows="5" type="text" name="message" />
+                                <span className="contact-input-err">{formError.message}</span>
+                            </label>
+                            <section className="terms-checkbox">
+                                <input type="checkbox" name="checkbox" required />
+                                <span>I accept the <Link to="terms">Terms</Link></span>
+                            </section>
+                            <button type="submit">Submit</button>
+                        </form>}
                     <img src={ContactCover} alt="Contact us" />
                 </section>
             </article>
