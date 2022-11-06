528f240bd25ae10f2cf1815ee53ffb288db392f5
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-11-06 21:14:52 +0400

    added scroll to top functionality when chnaging routes

--- src/components/dashboard-navigation/DashboardNavigation.js
+++ src/components/dashboard-navigation/DashboardNavigation.js
@@ -10,6 +10,7 @@ import { ReactComponent as Logout } from '../../assets/images/dashboard-icons/lo
 import ExpenselessLogo from '../../assets/images/expenseless-logo.jpg'
 import { useState } from "react";
 import Hamburger from "../hamburger/Hamburger";
+import ScrollToTop from "../../helpers/ScrollToTop";
 
 const DashboardNavigation = (props) => {
     const [responsive, setResponsive] = useState(false);
@@ -25,37 +26,37 @@ const DashboardNavigation = (props) => {
             <div className="sidebar-items">
                 <ul className="menu">
                     <li>
-                        <Link to="" onClick={() => { toggleMenu(); props.resetForm() }}>
+                        <Link to="" onClick={() => { toggleMenu(); props.resetForm(); ScrollToTop() }}>
                             <Home className="icon" />
                             <span className="item-name">Home</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="accounts" onClick={() => { toggleMenu(); props.resetForm() }}>
+                        <Link to="accounts" onClick={() => { toggleMenu(); props.resetForm(); ScrollToTop() }}>
                             <Accounts className="icon" />
                             <span className="item-name">accounts</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="expenses" onClick={() => { toggleMenu(); props.resetForm() }}>
+                        <Link to="expenses" onClick={() => { toggleMenu(); props.resetForm(); ScrollToTop() }}>
                             <Expenses className="icon" />
                             <span className="item-name">expenses</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="budget" onClick={() => { toggleMenu(); props.resetForm() }}>
+                        <Link to="budget" onClick={() => { toggleMenu(); props.resetForm(); ScrollToTop() }}>
                             <Budget className="icon" />
                             <span className="item-name">budget</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="statistics" onClick={() => { toggleMenu(); props.resetForm() }}>
+                        <Link to="statistics" onClick={() => { toggleMenu(); props.resetForm(); ScrollToTop() }}>
                             <Statistics className="icon" />
                             <span className="item-name">statistics</span>
                         </Link>
                     </li>
                     <li>
-                        <Link to="profile" onClick={() => { toggleMenu(); props.resetForm() }}>
+                        <Link to="profile" onClick={() => { toggleMenu(); props.resetForm(); ScrollToTop() }}>
                             <Profile className="icon" />
                             <span className="item-name">profile</span>
                         </Link>
@@ -64,7 +65,7 @@ const DashboardNavigation = (props) => {
                 <div className="bottom-content">
                     <li className="">
                         <Link to="/" onClick={props.logout}>
-                            <button className="logout-button" onClick={() => { props.logout(); toggleMenu(); props.resetForm(); }}>
+                            <button className="logout-button" onClick={() => { props.logout(); toggleMenu(); props.resetForm(); ScrollToTop() }}>
                                 <Logout className="icon" />
                                 <span className="item-name">Logout</span>
                             </button>
